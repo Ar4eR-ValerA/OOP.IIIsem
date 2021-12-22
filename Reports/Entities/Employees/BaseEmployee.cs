b1c9a54df82ce4ff4df44128748c70b79dabd549
Valery Shevchenko <ar4er.valera@gmail.com>
2021-12-23 01:04:56 +0300

    feat: employees entities added [3]

--- Reports/Entities/Employees/BaseEmployee.cs
+++ Reports/Entities/Employees/BaseEmployee.cs
@@ -1,4 +1,5 @@
 using System;
+using Reports.Dtos;
 using Reports.Tools;
 
 namespace Reports.Entities.Employees
@@ -9,9 +10,21 @@ namespace Reports.Entities.Employees
         {
             Id = Guid.NewGuid();
             Name = name ?? throw new ReportsExceptions("Name is null");
+            Active = true;
+        }
+
+        protected BaseEmployee(Guid id, string name, bool active)
+        {
+            Id = id;
+            Name = name ?? throw new ReportsExceptions("Name is null");
+            Active = active;
         }
 
         public Guid Id { get; private set; }
         public string Name { get; private set; }
+
+        public bool Active { get; set; }
+
+        public abstract BaseEmployeeDto GetDto();
     }
 }
