bc80eabec643b8255fc42a1c22b92d511b587d32
Valery Shevchenko <ar4er.valera@gmail.com>
2021-12-23 01:07:49 +0300

    feat: dtos added

---
+++ Reports/Dtos/BaseEmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Reports.Entities.Employees;
+using Reports.Tools;
+
+namespace Reports.Dtos
+{
+    public abstract class BaseEmployeeDto
+    {
+        protected BaseEmployeeDto(Guid id, string name, bool active)
+        {
+            Id = id;
+            Name = name ?? throw new ReportsExceptions("Name is null");
+            Active = active;
+        }
+        
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public bool Active { get; private set; }
+        public abstract BaseEmployee GetInstance();
+    }
+}
---
+++ Reports/Dtos/TeamLeadDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Reports.Entities.Employees;
+using Reports.Tools;
+
+namespace Reports.Dtos
+{
+    public class TeamLeadDto : BaseEmployeeDto
+    {
+        private readonly List<BaseEmployeeDto> _subordinates;
+
+        public TeamLeadDto(Guid id, string name, List<BaseEmployeeDto> subordinates, bool active)
+            : base(id, name ?? throw new ReportsExceptions("Name is null"), active)
+        {
+            _subordinates = subordinates ?? throw new ReportsExceptions("Subordinates are null");
+        }
+        
+        public IReadOnlyList<BaseEmployeeDto> Subordinates => _subordinates;
+
+        public override BaseEmployee GetInstance()
+        {
+            return new TeamLead(this);
+        }
+    }
+}
